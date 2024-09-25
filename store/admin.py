88c38de69055b1cc6ac31d583b2d6cdfc66488c3
Fiagapcoo <filipeaguilar08@gmail.com>
2024-09-26 00:43:11 +0100

    make tables appear in /admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Clientes)
+admin.site.register(Fornecedores)
+admin.site.register(Categoria)
+admin.site.register(Produtos)
+admin.site.register(Stock)
+admin.site.register(Promocoes)
+admin.site.register(Colaboradores)
+admin.site.register(Pedido)
+admin.site.register(Pagamentos)
+admin.site.register(ComprasForn)
