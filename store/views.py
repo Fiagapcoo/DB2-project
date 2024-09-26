54bcb2092cf50ddcb38a2f87cf0c08a86082f627
Fiagapcoo <filipeaguilar08@gmail.com>
2024-09-26 01:24:12 +0100

    add Product List just to see

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'store.html')
+    products = Produtos.objects.all()
+    return render(request, 'store.html', {'products': products})
