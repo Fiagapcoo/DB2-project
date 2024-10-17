dd2b7234ac764c082f14772517825a0f079d5a39
Fiagapcoo <filipeaguilar08@gmail.com>
2024-10-17 17:49:52 +0100

    login page almost ready (frontend only)

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 # Create your views here.
+
+def index(request):
+    #check if the user is authenticated if so render the home page
+    return redirect('login')
+
 
 def login(request):
     return render(request, 'login.html')
