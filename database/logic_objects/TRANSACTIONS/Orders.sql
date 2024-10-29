b5c8fc3975223836c6cacf71d903714b436afbb7
Fiagapcoo <filipeaguilar08@gmail.com>
2024-10-29 04:55:04 +0000

    fix in Orders logic objects

--- database/logic_objects/TRANSACTIONS/Orders.sql
+++ database/logic_objects/TRANSACTIONS/Orders.sql
@@ -44,37 +44,3 @@ BEGIN
     WHERE OrderID = p_OrderID;
 END;
 $$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION TRANSACTIONS.payment_insert_trigger_func()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Check if user exists
-    IF NOT EXISTS (
-        SELECT 1
-        FROM HR.Users
-        WHERE UserID = NEW.UserID
-    ) THEN
-        RAISE EXCEPTION 'User with ID % does not exist', NEW.UserID;
-    END IF;
-
-    -- Check if order exists
-    IF NOT EXISTS (
-        SELECT 1
-        FROM TRANSACTIONS.Orders
-        WHERE OrderID = NEW.OrderID
-    ) THEN
-        RAISE EXCEPTION 'Order with ID % does not exist', NEW.OrderID;
-    END IF;
-
-    -- Set the PaymentDate timestamp
-    NEW.PaymentDate := CURRENT_TIMESTAMP;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER payment_insert_trigger
-BEFORE INSERT ON TRANSACTIONS.Payments
-FOR EACH ROW
-EXECUTE PROCEDURE TRANSACTIONS.payment_insert_trigger_func();
