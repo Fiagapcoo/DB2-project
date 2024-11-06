de73590bd6a474b9542677e63e85a4c72f467dea
KingOfDeuses <ficavouga04@gmail.com>
2024-11-06 14:08:29 +0000

    Tob_bar, Filter_card, Navbar, Brand, Product_card (Tudo 100% check, menos o Filter_card)

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product_card", views.product_card, name="product_card"),
+    path("top_bar", views.top_bar, name="top_bar"),
+    path("navbar", views.navbar, name="navbar"),
+    path("brand", views.brand, name="brand"),
+    path("filter_card", views.filter_card, name="filter_card"),
 ]
--- store/views.py
+++ store/views.py
@@ -5,3 +5,18 @@ from .models import *
 def index(request):
     products = Produtos.objects.all()
     return render(request, 'store.html', {'products': products})
+
+def product_card(request):
+    return render(request, 'product_card.html')
+
+def top_bar(request):
+    return render(request, 'top_bar.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
+
+def brand(request):
+    return render(request, 'brand.html')
+
+def filter_card(request):
+    return render(request, 'filter_card.html')
