efccb4fc75ce6cd60fa6d7176d94b7b58aac2bda
Hazyc <hazycgaming@gmail.com>
2024-11-13 15:35:28 +0000

    Página Registo User

---
+++ autenticacao/static/autenticacao/js/register.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const successModal = document.getElementById("successModal");
+    const closeModalBtn = document.getElementById("closeModalBtn");
+
+    // Função para abrir o modal de sucesso
+    function openSuccessModal() {
+        successModal.style.display = "flex";
+    }
+
+    // Função para redirecionar para a página de login ao fechar o modal
+    function redirectToLogin() {
+        successModal.style.display = "none";
+        window.location.href = "{% url 'login' %}"; // Redireciona para a página de login
+    }
+
+    // Evento de envio do formulário
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Previne o envio imediato do formulário
+
+        // Verifica se todos os campos estão válidos de acordo com os patterns
+        if (form.checkValidity()) {
+            openSuccessModal(); // Exibe o modal de sucesso
+        } else {
+            form.reportValidity(); // Mostra os erros de validação
+        }
+    });
+
+    // Evento para redirecionar ao clicar no botão "Fazer Login"
+    closeModalBtn.addEventListener("click", redirectToLogin);
+});
