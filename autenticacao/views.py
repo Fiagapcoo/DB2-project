67e1c855ae5e9250a6f80eeb8d0e64efc73983d1
Fiagapcoo <filipeaguilar08@gmail.com>
2024-11-13 22:17:27 +0000

    fix register page

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from django.db import connection
 from encryption.encrypt_decrypt import encrypt_string
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,19 +14,30 @@ def login(request):
     #check if the user is authenticated if so render the home page
     return render(request, 'login.html')
 
-def register(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = encrypt_string(request.POST.get('email'))
-        password = encrypt_string(request.POST.get('password'))
-        full_name = f"{first_name} {last_name}"
-        
 
-        with connection.cursor() as cursor:
-            cursor.execute("CALL HR.InsertUser(%s, %s, %s, %s)", [full_name, '555-1234', email, password])
-        
+
+def register(request):
+    print("register")
+    if request.method == 'POST':
+        print("register post")
+        try:
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            email = encrypt_string(request.POST.get('email'))
+            password = encrypt_string(request.POST.get('password'))
+            phone_number = encrypt_string(request.POST.get('phone_number'))
+            full_name = f"{first_name} {last_name}"
+
+            with connection.cursor() as cursor:
+                cursor.execute("CALL HR.InsertUser(%s, %s, %s, %s)", [full_name, phone_number, email, password])
+
+            messages.success(request, "Account created successfully!")
+
+        except Exception as e:
+            messages.error(request, f"Error creating account: {e}")
+
     return render(request, 'register.html')
+
 def password_recovery(request):
     if request.method == 'POST':
         email = request.POST.get('email')
